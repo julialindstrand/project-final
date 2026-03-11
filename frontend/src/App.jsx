498420cc36e3a33f461784a9361dccfb200ca17f
Julia <juliakeo@pm.me>
2026-03-11 19:49:58 +0100

    Added useContext to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { GlobalStyle } from "./styling/GlobalStyles"
 import { LoginForm } from "./components/login"
