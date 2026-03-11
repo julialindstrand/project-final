fa704577a7e87c2d448c65c7cdd1773925961ca2
Julia <juliakeo@pm.me>
2026-03-11 18:13:41 +0100

    Fixes spellingerror

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ app.use(
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
-const mongoUri = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/final-project"
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/final-project"
 mongoose
-  .connect(mongoUri, {
+  .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
