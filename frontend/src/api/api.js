d0c02003093db32a2db3bde3fbad90812c59fea9
Julia <juliakeo@pm.me>
2026-03-11 16:54:27 +0100

    Adds renderlink hardcoded

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+export const API_URL = import.meta.env.VITE_API_URL || "https://api.render.com/deploy/srv-d6onvbqa214c73bfjus0?key=kG1oUUp7PgU"
 
 
 export const fetchJson = async (endpoint, options = {}) => {
